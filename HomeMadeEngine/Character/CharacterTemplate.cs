d35d26f182fe49dd8cae264166e2da75b8ff04c6
Emile Boucher <1327897@cstjean.qc.ca>
2020-08-18 08:35:26 -0400

    Class creation

--- HomeMadeEngine/Character/CharacterTemplate.cs
+++ HomeMadeEngine/Character/CharacterTemplate.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using HomeMadeEngine.Action;
+using HomeMadeEngine.Character;
 using HomeMadeEngine.Math;
 
 namespace HomeMadeEngine.Character
--- HomeMadeEngine/Character/StatsTemplate.cs
+++ HomeMadeEngine/Character/StatsTemplate.cs
@@ -7,6 +7,35 @@ namespace HomeMadeEngine.Character
     [Serializable]
     public class StatsTemplate
     {
+        //------------------------------------------------------------------------------------------------------------
+        // FIELDS
+        //____________________________________________________________________________________________________________
+        public string name { get; set; }
+        public StatType type { get; set; }
+        public DamageType? dmg { get; set; }
+        public double flat { get; set; }
+        public double multi { get; set; }
 
+        //------------------------------------------------------------------------------------------------------------
+        // CONSTRUCTORS
+        //____________________________________________________________________________________________________________
+        // MAIN CONSTRUCTORS
+        //------------------------------------------------------------------------------------------------------------
+        public StatsTemplate(string p_name, StatType p_stat, DamageType? p_dmg, double p_flat, double p_multi)
+        {
+            this.name = p_name;
+            this.type = p_stat;
+            this.dmg = p_dmg;
+            this.flat = p_flat;
+            this.multi = p_multi;
+        }
+
+        // SHORTCUT CONSTRUCTORS
+        //------------------------------------------------------------------------------------------------------------
+        // public StatsTemplate(string p_name, StatType p_stat, double p_flat, double p_multi) : this(p_name, p_stat, null, p_flat, p_multi);
+
+        //------------------------------------------------------------------------------------------------------------
+        // FUNCTIONS
+        //____________________________________________________________________________________________________________
     }
 }
