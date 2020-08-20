2cc237aaf07928d9ec16a6716dfce7e447fd2716
Emile Boucher <1327897@cstjean.qc.ca>
2020-08-20 08:15:12 -0400

    Fix 2.0

--- EB_Library/Serialization/CharacterTemplateConverterJson.cs
+++ EB_Library/Serialization/CharacterTemplateConverterJson.cs
@@ -14,153 +14,153 @@ namespace ConsoleGamePlayer.Serialization
         }
         public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
         {
+            CharacterTemplate charT;
             if (value != null)
-            {
-                var charT = (CharacterTemplate)value;
-                writer.WriteStartObject();
-                writer.WritePropertyName("Level");
-                serializer.Serialize(writer, charT.Level);
+                charT = (CharacterTemplate)value;
+            else
+                charT = new CharacterTemplate();
+            writer.WriteStartObject();
+            writer.WritePropertyName("Level");
+            serializer.Serialize(writer, charT.Level);
 
-                writer.WritePropertyName("Experience");
-                serializer.Serialize(writer, charT.Experience);
+            writer.WritePropertyName("Experience");
+            serializer.Serialize(writer, charT.Experience);
 
-                writer.WritePropertyName("CurrentHp");
-                serializer.Serialize(writer, charT.CurrentHp);
+            writer.WritePropertyName("CurrentHp");
+            serializer.Serialize(writer, charT.CurrentHp);
 
-                writer.WritePropertyName("MaxHp");
-                serializer.Serialize(writer, charT.MaxHp);
+            writer.WritePropertyName("MaxHp");
+            serializer.Serialize(writer, charT.MaxHp);
 
-                writer.WritePropertyName("Shield");
-                serializer.Serialize(writer, charT.Shield);
+            writer.WritePropertyName("Shield");
+            serializer.Serialize(writer, charT.Shield);
 
-                writer.WritePropertyName("ShieldTimer");
-                serializer.Serialize(writer, charT.ShieldTimer);
+            writer.WritePropertyName("ShieldTimer");
+            serializer.Serialize(writer, charT.ShieldTimer);
 
-                writer.WritePropertyName("RessourceType");
-                serializer.Serialize(writer, charT.RessourceType);
+            writer.WritePropertyName("RessourceType");
+            serializer.Serialize(writer, charT.RessourceType);
 
-                writer.WritePropertyName("MaxRessource");
-                serializer.Serialize(writer, charT.MaxRessource);
+            writer.WritePropertyName("MaxRessource");
+            serializer.Serialize(writer, charT.MaxRessource);
 
-                writer.WritePropertyName("IsDead");
-                serializer.Serialize(writer, charT.IsDead);
+            writer.WritePropertyName("IsDead");
+            serializer.Serialize(writer, charT.IsDead);
 
-                writer.WritePropertyName("Stats");
-                writer.WriteStartArray();
-                writer.WriteStartObject();
-                writer.WritePropertyName("Name");
-                serializer.Serialize(writer, charT.Stats[0].Name);
-                writer.WritePropertyName("Stat");
-                serializer.Serialize(writer, charT.Stats[0].Stat);
-                writer.WritePropertyName("Dmg");
-                serializer.Serialize(writer, charT.Stats[0].Dmg);
-                writer.WritePropertyName("Flat");
-                serializer.Serialize(writer, charT.Stats[0].Flat);
-                writer.WritePropertyName("Multi");
-                serializer.Serialize(writer, charT.Stats[0].Multi);
-                writer.WriteEndObject();
-                writer.WriteEndArray();
+            writer.WritePropertyName("Stats");
+            writer.WriteStartArray();
+            writer.WriteStartObject();
+            writer.WritePropertyName("Name");
+            serializer.Serialize(writer, charT.Stats[0].Name);
+            writer.WritePropertyName("Stat");
+            serializer.Serialize(writer, charT.Stats[0].Stat);
+            writer.WritePropertyName("Dmg");
+            serializer.Serialize(writer, charT.Stats[0].Dmg);
+            writer.WritePropertyName("Flat");
+            serializer.Serialize(writer, charT.Stats[0].Flat);
+            writer.WritePropertyName("Multi");
+            serializer.Serialize(writer, charT.Stats[0].Multi);
+            writer.WriteEndObject();
+            writer.WriteEndArray();
 
-                writer.WritePropertyName("Equipements");
-                writer.WriteStartArray();
-                writer.WriteStartObject();
-                writer.WritePropertyName("Name");
-                serializer.Serialize(writer, charT.Equipements[0].Name);
-                writer.WritePropertyName("Dmg");
-                serializer.Serialize(writer, charT.Equipements[0].Slot);
-                writer.WritePropertyName("Stat");
-                serializer.Serialize(writer, charT.Equipements[0].Rarity);
-                writer.WriteStartArray();
-                writer.WritePropertyName("Flat");
-                serializer.Serialize(writer, charT.Equipements[0].Stats);
-                writer.WriteEndArray();
-                writer.WriteEndObject();
-                writer.WriteEndArray();
+            writer.WritePropertyName("Equipements");
+            writer.WriteStartArray();
+            writer.WriteStartObject();
+            writer.WritePropertyName("Name");
+            serializer.Serialize(writer, charT.Equipements[0].Name);
+            writer.WritePropertyName("Dmg");
+            serializer.Serialize(writer, charT.Equipements[0].Slot);
+            writer.WritePropertyName("Stat");
+            serializer.Serialize(writer, charT.Equipements[0].Rarity);
+            writer.WriteStartArray();
+            writer.WritePropertyName("Flat");
+            serializer.Serialize(writer, charT.Equipements[0].Stats);
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+            writer.WriteEndArray();
 
-                writer.WritePropertyName("Actions");
-                writer.WriteStartArray();
-                writer.WriteStartObject();
-                writer.WritePropertyName("Name");
-                serializer.Serialize(writer, charT.Actions[0].Name);
-                writer.WritePropertyName("Stat");
-                serializer.Serialize(writer, charT.Actions[0].Cost);
-                writer.WritePropertyName("Dmg");
-                serializer.Serialize(writer, charT.Actions[0].Index);
-                writer.WriteEndObject();
-                writer.WriteEndArray();
+            writer.WritePropertyName("Actions");
+            writer.WriteStartArray();
+            writer.WriteStartObject();
+            writer.WritePropertyName("Name");
+            serializer.Serialize(writer, charT.Actions[0].Name);
+            writer.WritePropertyName("Stat");
+            serializer.Serialize(writer, charT.Actions[0].Cost);
+            writer.WritePropertyName("Dmg");
+            serializer.Serialize(writer, charT.Actions[0].Index);
+            writer.WriteEndObject();
+            writer.WriteEndArray();
 
-                writer.WritePropertyName("Buffs");
-                writer.WriteStartArray();
-                writer.WriteStartObject();
-                writer.WritePropertyName("Name");
-                serializer.Serialize(writer, charT.Buffs[0].Name);
-                writer.WritePropertyName("Timer");
-                serializer.Serialize(writer, charT.Buffs[0].Timer);
-                writer.WriteStartArray();
-                writer.WritePropertyName("Stat");
-                serializer.Serialize(writer, charT.Buffs[0].Stat);
-                writer.WriteEndArray();
-                writer.WriteEndObject();
-                writer.WriteEndArray();
+            writer.WritePropertyName("Buffs");
+            writer.WriteStartArray();
+            writer.WriteStartObject();
+            writer.WritePropertyName("Name");
+            serializer.Serialize(writer, charT.Buffs[0].Name);
+            writer.WritePropertyName("Timer");
+            serializer.Serialize(writer, charT.Buffs[0].Timer);
+            writer.WriteStartArray();
+            writer.WritePropertyName("Stat");
+            serializer.Serialize(writer, charT.Buffs[0].Stat);
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+            writer.WriteEndArray();
 
-                writer.WritePropertyName("Debuffs");
-                writer.WriteStartArray();
-                writer.WriteStartObject();
-                writer.WritePropertyName("Name");
-                serializer.Serialize(writer, charT.Debuffs[0].Name);
-                writer.WritePropertyName("Timer");
-                serializer.Serialize(writer, charT.Debuffs[0].Timer);
-                writer.WriteStartArray();
-                writer.WritePropertyName("Stat");
-                serializer.Serialize(writer, charT.Debuffs[0].Stat);
-                writer.WriteEndArray();
-                writer.WriteEndObject();
-                writer.WriteEndArray();
+            writer.WritePropertyName("Debuffs");
+            writer.WriteStartArray();
+            writer.WriteStartObject();
+            writer.WritePropertyName("Name");
+            serializer.Serialize(writer, charT.Debuffs[0].Name);
+            writer.WritePropertyName("Timer");
+            serializer.Serialize(writer, charT.Debuffs[0].Timer);
+            writer.WriteStartArray();
+            writer.WritePropertyName("Stat");
+            serializer.Serialize(writer, charT.Debuffs[0].Stat);
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+            writer.WriteEndArray();
 
-                writer.WritePropertyName("Postion");
-                writer.WriteStartObject();
-                writer.WritePropertyName("X");
-                serializer.Serialize(writer, charT.Position.X);
-                writer.WritePropertyName("Y");
-                serializer.Serialize(writer, charT.Position.Y);
-                writer.WritePropertyName("Z");
-                serializer.Serialize(writer, charT.Position.Z);
-                writer.WriteEndObject();
+            writer.WritePropertyName("Postion");
+            writer.WriteStartObject();
+            writer.WritePropertyName("X");
+            serializer.Serialize(writer, charT.Position.X);
+            writer.WritePropertyName("Y");
+            serializer.Serialize(writer, charT.Position.Y);
+            writer.WritePropertyName("Z");
+            serializer.Serialize(writer, charT.Position.Z);
+            writer.WriteEndObject();
 
-                writer.WritePropertyName("Velocity");
-                writer.WriteStartObject();
-                writer.WritePropertyName("X");
-                serializer.Serialize(writer, charT.Velocity.X);
-                writer.WritePropertyName("Y");
-                serializer.Serialize(writer, charT.Velocity.Y);
-                writer.WritePropertyName("Z");
-                serializer.Serialize(writer, charT.Velocity.Z);
-                writer.WriteEndObject();
+            writer.WritePropertyName("Velocity");
+            writer.WriteStartObject();
+            writer.WritePropertyName("X");
+            serializer.Serialize(writer, charT.Velocity.X);
+            writer.WritePropertyName("Y");
+            serializer.Serialize(writer, charT.Velocity.Y);
+            writer.WritePropertyName("Z");
+            serializer.Serialize(writer, charT.Velocity.Z);
+            writer.WriteEndObject();
 
-                writer.WritePropertyName("Acceleration");
-                writer.WriteStartObject();
-                writer.WritePropertyName("X");
-                serializer.Serialize(writer, charT.Acceleration.X);
-                writer.WritePropertyName("Y");
-                serializer.Serialize(writer, charT.Acceleration.Y);
-                writer.WritePropertyName("Z");
-                serializer.Serialize(writer, charT.Acceleration.Z);
-                writer.WriteEndObject();
+            writer.WritePropertyName("Acceleration");
+            writer.WriteStartObject();
+            writer.WritePropertyName("X");
+            serializer.Serialize(writer, charT.Acceleration.X);
+            writer.WritePropertyName("Y");
+            serializer.Serialize(writer, charT.Acceleration.Y);
+            writer.WritePropertyName("Z");
+            serializer.Serialize(writer, charT.Acceleration.Z);
+            writer.WriteEndObject();
 
-                writer.WritePropertyName("Gravity");
-                writer.WriteStartObject();
-                writer.WritePropertyName("X");
-                serializer.Serialize(writer, charT.Gravity.X);
-                writer.WritePropertyName("Y");
-                serializer.Serialize(writer, charT.Gravity.Y);
-                writer.WritePropertyName("Z");
-                serializer.Serialize(writer, charT.Gravity.Z);
-                writer.WriteEndObject();
+            writer.WritePropertyName("Gravity");
+            writer.WriteStartObject();
+            writer.WritePropertyName("X");
+            serializer.Serialize(writer, charT.Gravity.X);
+            writer.WritePropertyName("Y");
+            serializer.Serialize(writer, charT.Gravity.Y);
+            writer.WritePropertyName("Z");
+            serializer.Serialize(writer, charT.Gravity.Z);
+            writer.WriteEndObject();
 
-                writer.WriteEndObject();
-            }
+            writer.WriteEndObject();
         }
-
         public override object ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
         {
             if (existingValue != null)
