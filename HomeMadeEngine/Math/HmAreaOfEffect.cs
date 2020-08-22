e02c0ae435cc1c510c0d245ced725bf67d3b5d6d
Emile Boucher <1327897@cstjean.qc.ca>
2020-08-21 20:17:54 -0400

    correction

--- HomeMadeEngine/Math/HmAreaOfEffect.cs
+++ HomeMadeEngine/Math/HmAreaOfEffect.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Text;
 
 namespace HomeMadeEngine.Math
@@ -13,9 +14,7 @@ namespace HomeMadeEngine.Math
         public int X { get; private set; }
         public int Y { get; private set; }
         public int Z { get; private set; }
-        public List<bool> Xspace { get; private set; }
-        public List<bool> Yspace { get; private set; }
-        public List<bool> Zspace { get; private set; }
+        public List<List<List<bool>>> Space { get; private set; }
 
         //------------------------------------------------------------------------------------------------------------
         // CONSTRUCTORS
@@ -33,37 +32,37 @@ namespace HomeMadeEngine.Math
             this.X = p_x;
             this.Y = p_y;
             this.Z = p_z;
-            this.Xspace = new List<bool>();
-            this.Yspace = new List<bool>();
-            this.Zspace = new List<bool>();
-            for(int i = p_x; i > 0; i--)
+            this.Space = new List<List<List<bool>>>();
+            for(int x = p_x; x > 0; x--)
             {
-                this.Xspace.Add(true);
-            }
-            for (int i = p_y; i > 0; i--)
-            {
-                this.Yspace.Add(true);
-            }
-            for (int i = p_z; i > 0; i--)
-            {
-                this.Zspace.Add(true);
+                List<List<bool>> Yrow = new List<List<bool>>();
+                for (int y = p_y; y > 0; y--) 
+                {
+                    List<bool> Zrow = new List<bool>();
+                    for (int z = p_z; z > 0; z--)
+                    {
+                        Zrow.Add(true);
+                    }
+                    if (p_z <= 0)
+                        Zrow.Add(true);
+                    Yrow.Add(Zrow);
+                }
+                this.Space.Add(Yrow);
             }
             this.Dimension = 1 + ((this.Y > 0) ? 1 : 0) + ((this.Z > 0) ? 1 : 0);
         }
-        public HmAreaOfEffect(List<bool> p_x, List<bool> p_y, List<bool> p_z)
+        public HmAreaOfEffect(List<List<List<bool>>>p_space)
         {
-            if (p_x.Count <= 0)
+            if (p_space.Count <= 0)
                 throw new ArgumentException("X MUST BE HIGHER THAN 0");
-            if (p_y.Count < 0)
+            if (p_space[0].Count < 0)
                 throw new ArgumentException("Y MUST BE POSITIVE");
-            if (p_z.Count < 0)
+            if (p_space[0][0].Count < 0)
                 throw new ArgumentException("Z MUST BE POSITIVE");
-            this.X = p_x.Count;
-            this.Y = p_y.Count;
-            this.Z = p_z.Count;
-            this.Xspace = p_x;
-            this.Yspace = p_y;
-            this.Zspace = p_z;
+            this.X = p_space.Count;
+            this.Y = p_space[0].Count;
+            this.Z = p_space[0][0].Count;
+            this.Space = p_space;
             this.Dimension = 1 + ((this.Y > 0) ? 1 : 0) + ((this.Z > 0) ? 1 : 0);
         }
 
@@ -71,9 +70,6 @@ namespace HomeMadeEngine.Math
         //------------------------------------------------------------------------------------------------------------
         public HmAreaOfEffect(int p_x, int p_y) : this(p_x, p_y, 0) { }
         public HmAreaOfEffect(int p_x) : this(p_x, 0) { }
-        public HmAreaOfEffect(List<bool> p_x, List<bool> p_y) : this(p_x, p_y, new List<bool>()) { }
-        public HmAreaOfEffect(List<bool> p_x):this(p_x, new List<bool>()) { }
-
 
         //------------------------------------------------------------------------------------------------------------
         // FUNCTIONS
